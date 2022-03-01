0176d0f5f3d2675c2045fbda934be5ca3d28927e
Mehmet Can Şimşek <mehmetsimsek>
2022-03-01 21:58:57 +0300

    Initial Commit

---
+++ InstaCloneFarebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFarebase
+//
+//  Created by Mehmet Can Şimşek on 1.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
